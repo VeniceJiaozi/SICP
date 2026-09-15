cbffab281a46d2f1d9d6767c018a3fb74f0038b7
VeniceJiaozi <2366759931@qq.com>
2026-09-15 20:24:58 +0800

    2026.9.15

---
+++ week 1/lab00-Code/lab00.py
@@ -0,0 +1,33 @@
+def twenty_twenty_six():
+    """Come up with the most creative expression that evaluates to 2026,
+    using only numbers and the +, *, and - operators.
+
+    Expected result:
+    >>> twenty_twenty_six()
+    2026
+    """
+    return 1013*2
+
+
+def sum(a, b):
+    """Compute the sum of a and b.
+
+    Expected result:
+    >>> sum(1, 2)
+    3
+    >>> sum(3, 8)
+    11
+    """
+    return a+b
+
+
+def diff_square(a, b):
+    """Compute the difference of square a and square b.
+
+    Expected result:
+    >>> diff_square(3, 2)
+    5
+    >>> diff_square(3, 4)
+    -7
+    """
+    return a*a-b*b
